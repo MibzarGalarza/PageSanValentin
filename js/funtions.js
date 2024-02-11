6d994b45f13fefc5219704e903eb661b110732b7
MibzarGalarza <mibzar@advantedigital.com>
2024-02-11 21:06:09 +0100

    first commit

---
+++ js/funtions.js
@@ -0,0 +1,22 @@
+function mostrarContenido() {
+    var contenido = document.getElementById("contenido");
+    var contenido2 = document.getElementById("contenido-2");
+    var buttonLeer = document.getElementById("button-leer");
+
+    if (contenido.style.display === "none") {
+        var usuario = prompt("Usuario:");
+        var contraseña = prompt("Contraseña:");
+        if (usuario === "Andrea&Chichar" && contraseña === "090623") {
+            contenido.style.display = "block";
+            contenido2.style.display = "none";
+            buttonLeer.textContent = "Back";
+        } else {
+            alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
+        }
+
+    } else {
+        contenido.style.display = "none";
+        contenido2.style.display = "block";
+        buttonLeer.textContent = "Leer carta";
+    }
+}
